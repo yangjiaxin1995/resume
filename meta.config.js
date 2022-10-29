8f919c8079398a8a58ea0a0746bf8cf0732a2315
Thresh <601426683@qq.com>
2022-10-29 11:30:14 +0800

    init

---
+++ meta.config.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'Thresh(杨佳鑫)',
+  skills: [
+    'Javascript',
+    'Typescript',
+    'React',
+    'Solidjs',
+    'Webpack',
+    'vite',
+    'umi',
+    'qiankun',
+  ],
+};
